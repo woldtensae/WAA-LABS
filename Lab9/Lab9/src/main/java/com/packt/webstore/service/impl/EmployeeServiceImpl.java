1da8c936955e8245569ee971bd844771bdb9e3d7
woldt <woldtensae.aman@gmail.com>
2017-11-12 20:20:31 -0600

    final lab 9 commit

--- Lab9/Lab9/src/main/java/com/packt/webstore/service/impl/EmployeeServiceImpl.java
+++ Lab9/Lab9/src/main/java/com/packt/webstore/service/impl/EmployeeServiceImpl.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 
 import com.packt.webstore.domain.Employee;
 import com.packt.webstore.repository.EmployeeRepository;
 import com.packt.webstore.service.EmployeeService;
 
 @Service
+@Transactional
 public class EmployeeServiceImpl implements EmployeeService{
 
 	@Autowired
